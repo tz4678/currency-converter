60436e03e888e87fac36286f795ea53a514a6d5d
Kenneth McCormick <tz4678@gmail.com>
2020-04-04 17:37:59 +0300

    при ошибке возвращается объект с полем error

--- currency_converter/errors.py
+++ currency_converter/errors.py
@@ -3,20 +3,20 @@ from dataclasses import dataclass
 
 @dataclass
 class ApiError(Exception):
-    message: str = 'Bad Request'
+    error_message: str = 'Bad Request'
     status_code: int = 400
 
     def get_response(self):
-        return self.status_code, {'code': self.status_code, 'message': self.message}
+        return self.status_code, {'error': self.error_message}
 
 
 class NotFound(ApiError):
 
-    def __init__(self, message: str = 'Not Found') -> None:
-        super().__init__(message=message, status_code=404)
+    def __init__(self, error_message: str = 'Not Found') -> None:
+        super().__init__(error_message=error_message, status_code=404)
 
 
 class InternalError(ApiError):
 
     def __init__(self, e) -> None:
-        super().__init__(message=str(e), status_code=500)
+        super().__init__(error_message=str(e), status_code=500)
